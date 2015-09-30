0e5bb6ef8bcb4c5583a2e26700d548de279aa5b8
Tim Gittos <timg@spiceworks.com>
2015-09-30 18:22:03 -0500

    Fleshed out the content_test show page a little

---
+++ spec/features/viewing_content_test_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "user can view content test" do
+
+  before do
+    @site = FactoryGirl.create(:site, name: "Google Status Page")
+    @content_test = FactoryGirl.create(:content_test, site: @site, content: "foobar")
+    
+    # 9 times, cause creating the initial content test creates a test_status
+    9.times do
+      FactoryGirl.create(:test_status, content_test: @content_test, result: true)
+    end
+
+    visit site_content_test_path(@site, @content_test)
+  end
+
+  scenario "with the site name" do
+    expect(page).to have_content @site.name 
+  end
+
+  scenario "with the matching criteria" do
+    expect(page).to have_content "#{@content_test.comparison} \"#{@content_test.content}\""
+  end
+
+  scenario "with history of results" do
+    expect(find('#results ul')).to have_selector('li', count: 10)
+    expect(find('#results')).to have_content('OK')
+  end
+
+end
