b9f6c0ef7d4ae035b67f7e2222200d5781866915
Tim Gittos <timg@spiceworks.com>
2015-10-02 01:18:50 -0500

    Crank down the domain check to once / 15 mins

--- config/initializers/rufus_scheduler.rb
+++ config/initializers/rufus_scheduler.rb
@@ -4,6 +4,6 @@ scheduler = Rufus::Scheduler.singleton
 
 Rails.logger.info "Scheduling SiteMonitor to run every 15m"
 SiteMonitor.update!
-scheduler.every("5m") do
+scheduler.every("15m") do
   SiteMonitor.update!
 end
