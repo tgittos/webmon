5d0f1b1a1653c267e15bb69edfcdec51042504cc
Tim Gittos <timg@spiceworks.com>
2015-11-24 13:12:08 -0600

    Added some better validation
    1. Validate we can visit the site before we add it
    2. Validate that the site has a sane http/https scheme.

--- app/models/site.rb
+++ app/models/site.rb
@@ -7,6 +7,9 @@ class Site < ActiveRecord::Base
 
   validates :name, presence: true
   validates :url, presence: true, uniqueness: { scope: :user_id }
+  validate :can_reach_url
+
+  before_validation :add_missing_url_scheme
 
   scope :active, ->{ where(active: true) }
 
@@ -29,6 +32,19 @@ class Site < ActiveRecord::Base
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE # read into this
     end
     http.get(uri.request_uri)
+  end
+
+  def can_reach_url
+    begin
+      http_get(url)
+    rescue Exception => e
+      Rails.logger.debug("#{e.message}\n\t#{e.backtrace.join("\n\t")}")
+      errors.add(:url, "can't be reached from URL Up")
+    end
+  end
+
+  def add_missing_url_scheme
+    self.url = "http://#{self.url}" if self.url !~ /https?:\/\//
   end
 
 end
