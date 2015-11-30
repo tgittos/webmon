0b8c044e8b5b222fb3a9d171a88788cd65eb7f67
Tim Gittos <timg@spiceworks.com>
2015-11-30 16:49:59 -0600

    Dont run the site monitor on app boot

--- config/initializers/rufus_scheduler.rb
+++ config/initializers/rufus_scheduler.rb
@@ -8,7 +8,6 @@ end
 
 if in_server? && !ENV['WEBMON_NO_CHECK']
   Rails.logger.info "Scheduling SiteMonitor to run every 15m"
-  SiteMonitor.update!
   scheduler.every("15m") do
     SiteMonitor.update!
   end
