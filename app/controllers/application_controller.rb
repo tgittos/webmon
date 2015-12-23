d245b451d8bc7418da73094eca176df28ad2262d
Tim Gittos <timg@spiceworks.com>
2015-12-23 11:41:18 -0600

    Remove hardcoded user AGAIN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def ensure_logged_in
-    session["user"] = User.first
     redirect_to root_path unless session[:user]
     @user = User.find(session[:user]["id"])
   end
